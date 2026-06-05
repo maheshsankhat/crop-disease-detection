4980650ec43e24d4cff1821a4309d8db5451f7e3
mahesh sankhat <maheshsankhat21@gmail.com>
2026-06-05 16:50:59 +0530

    work on streamlit file

--- main.py
+++ main.py
@@ -51,9 +51,9 @@ elif(app_mode=="About"):
     #### About Dataset
     This dataset is recreated using offline augmentation from the original dataset. The original dataset can be found on this github repo. This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes. The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure. A new directory containing 33 test images is created later for prediction purpose.
     #### Content
-    1. Train 
-    2. Valid 
-    3. Test 
+    1. Train images
+    2. Valid images
+    3. Test images
 """)
     
 #Prediction Page
